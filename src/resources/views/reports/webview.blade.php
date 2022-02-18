a5274191a443d351b8e332c2f849ce4c442706ff
Loek Wetzels <loek.wetzels@gmail.com>
2022-02-18 11:51:03 +0100

    bugfix in webview when 0 items

--- src/resources/views/reports/webview.blade.php
+++ src/resources/views/reports/webview.blade.php
@@ -12,28 +12,38 @@
 
 		<h1 class="text-3xl font-bold mb-2">{{ $title }}</h1>
 
-		<table class="table-auto w-full border-2">
-			<thead class="text-xs text-left font-semibold uppercase text-gray-400 bg-gray-50 sticky top-0 border-2">
-				<tr>
-					@foreach ($items->first() as $key => $value)
-						<th class="p-2 whitespace-nowrap">
-							{!!  str_replace('.','<br/>',$key) !!}
-						</th>
+		@if( $items->count() == 0)
+
+			<div class="text-center">
+				<p class="text-gray-500">No items found</p>
+			</div>
+
+		@else
+
+			<table class="table-auto w-full border-2">
+				<thead class="text-xs text-left font-semibold uppercase text-gray-400 bg-gray-50 sticky top-0 border-2">
+					<tr>
+						@foreach ($items->first() as $key => $value)
+							<th class="p-2 whitespace-nowrap">
+								{!!  str_replace('.','<br/>',$key) !!}
+							</th>
+						@endforeach
+					</tr>
+				</thead>
+				<tbody class="text-sm divide-y divide-gray-100">
+					@foreach ($items as $item)
+					<tr>
+						@foreach ($item as $value)
+							<td class="p-2 whitespace-nowrap">
+								{{ $value }}
+							</td>
+						@endforeach
+					</tr>
 					@endforeach
-				</tr>
-			</thead>
-			<tbody class="text-sm divide-y divide-gray-100">
-				@foreach ($items as $item)
-				<tr>
-					@foreach ($item as $value)
-						<td class="p-2 whitespace-nowrap">
-							{{ $value }}
-						</td>
-					@endforeach
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
+				</tbody>
+			</table>
+
+		@endif
 
     </body>
 </html>
